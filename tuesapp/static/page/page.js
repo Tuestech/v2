9ff13e69660dcf6b76a006787068e33d85b6b907
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-06-02 12:02:58 -0500

    Added page class and css hidden

---
+++ tuesapp/static/page/page.js
@@ -0,0 +1,18 @@
+class Page {
+	static pageName
+	static pageBody
+
+	static updateVisibility() {
+		const activePageName = Nav.pages[Nav.activePage]
+		if (activePageName == this.pageName) {
+			this.pageBody.classList.toggle("hidden", false)
+		} else {
+			this.pageBody.classList.toggle("hidden", true)
+		}
+	}
+
+	static init() {
+		this.updateVisibility()
+		document.addEventListener("pageChange", this.updateVisibility)
+	}
+}
