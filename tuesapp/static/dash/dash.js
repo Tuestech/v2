b25b551d0e2bdcc0da79f53183e3f2e7357e7e3d
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-06-03 08:15:38 -0500

    Dash row 1 js

--- tuesapp/static/dash/dash.js
+++ tuesapp/static/dash/dash.js
@@ -5,8 +5,6 @@ class Dash extends Page {
 	}
 
 	static updateBasicInfoPanel() {
-		const panel = document.getElementById("date-display")
-
 		// Build Date String
 		const now = new Date()
 		const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -18,6 +16,21 @@ class Dash extends Page {
 		// Build Progress String (TODO)
 
 		// Build Timing String (TODO)
+	}
+
+	// pdone and pstarted are between 0 and 100
+	// pstarted doesn't include tasks that are done
+	static updateProgressBar(pdone, pstarted) {
+		const prefix = "transform: translateX("
+		document.getElementById("green-progress").setAttribute("style", prefix + pdone + "%);")
+		document.getElementById("yellow-progress").setAttribute("style", prefix + (pdone + pstarted) + "%);")
+	}
+
+	// pgood and pbad are between 0 and 100
+	static updateProgressBar(pgood, pbad) {
+		const prefix = "transform: translateX("
+		document.getElementById("green-timing").setAttribute("style", prefix + pgood + "%);")
+		document.getElementById("red-timing").setAttribute("style", prefix + (pgood + pbad) + "%);")
 	}
 	
 	static init() {
