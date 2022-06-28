a6cc97f62a54560998e35cc8fca918ddf4359a79
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-06-28 17:30:29 -0500

    Task bugfixes

--- tuesapp/static/task.js
+++ tuesapp/static/task.js
@@ -1,14 +1,19 @@
 class Task {
 	static keys = ["name", "course", "start", "end", "progress", "link"]
+	static types = ["s", "s", "d", "d", "n", "s"]
 	constructor(arr) {
 		for (let i = 0; i < Task.keys.length; i++) {
-			this[Task.keys[i]] = arr[i]
+			if (Task.types[i] != "d") {
+				this[Task.keys[i]] = arr[i]
+			} else {
+				this[Task.keys[i]] = new Date(arr[i])
+			}
 		}
 	}
 
 	toArray() {
 		let out = []
-		for (const key of keys) {
+		for (const key of Task.keys) {
 			out.push(this[key])
 		}
 		return out
