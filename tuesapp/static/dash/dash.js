a1d2b7e8ddccb31ffab3bb7ad38725d1b0353420
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-06-29 15:22:18 -0500

    Refactor with template literals

--- tuesapp/static/dash/dash.js
+++ tuesapp/static/dash/dash.js
@@ -9,7 +9,7 @@ class Dash extends Page {
 		const now = new Date()
 		const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 		const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-		let dateString = dayNames[now.getDay()] + ", " + monthNames[now.getMonth()] + " " + now.getDate()
+		let dateString = `${dayNames[now.getDay()]}, ${monthNames[now.getMonth()]} ${now.getDate()}`
 
 		document.getElementById("date-display").innerText = dateString
 
@@ -40,15 +40,14 @@ class Dash extends Page {
 	// pstarted doesn't include tasks that are done
 	static updateProgressBar(pdone, pstarted) {
 		const prefix = "transform: translateX("
-		document.getElementById("green-progress").setAttribute("style", prefix + pdone + "%);")
-		document.getElementById("yellow-progress").setAttribute("style", prefix + (pdone + pstarted) + "%);")
+		document.getElementById("green-progress").setAttribute("style", `transform: translateX(${pdone}%);`)
+		document.getElementById("yellow-progress").setAttribute("style", `transform: translateX(${pdone +pstarted}%);`)
 	}
 
 	// pgood and pbad are between 0 and 100
 	static updateTimingBar(pgood, pbad) {
-		const prefix = "transform: translateX("
-		document.getElementById("green-timing").setAttribute("style", prefix + pgood + "%);")
-		document.getElementById("red-timing").setAttribute("style", prefix + (pgood + pbad) + "%);")
+		document.getElementById("green-timing").setAttribute("style", `transform: translateX(${pgood}%);`)
+		document.getElementById("red-timing").setAttribute("style", `transform: translateX(${pgood + pbad}%);`)
 	}
 	
 	static init() {
