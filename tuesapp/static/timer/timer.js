c803669ea4b3ea1ba01224c66b907d4e793d9a71
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-08-24 20:35:21 -0500

    Activate timer buttons framework

--- tuesapp/static/timer/timer.js
+++ tuesapp/static/timer/timer.js
@@ -2,6 +2,16 @@ class Timer extends Page {
 	static init() {
 		this.pageName = "timer"
 		this.pageBody = document.getElementById("timer")
+
+		Timer.activateButtons()
+
 		super.init()
+	}
+
+	static activateButtons() {
+		const sequence = document.getElementById("sequence")
+		sequence.addEventListener("click", () => {
+			// TODO: Deactivate active button, activate current button, update time display
+		})
 	}
 }
