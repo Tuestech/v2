8953e255ea15d84503362487508db0da997b81f2
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-11-12 17:55:23 -0600

    Heroku fix

--- tuesday/settings.py
+++ tuesday/settings.py
@@ -1,8 +1,14 @@
 from pathlib import Path
+import django_heroku
+import os
+
+# Heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -148,3 +154,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
