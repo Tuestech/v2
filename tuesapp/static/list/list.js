b51a18db111e937d2397d68683d0e21dae19ade7
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2022-12-26 19:46:34 -0600

    Populate list panels

--- tuesapp/static/list/list.js
+++ tuesapp/static/list/list.js
@@ -5,18 +5,31 @@ class List extends Page {
 		super.init()
 	}
 
-	static updateToDo(tasks) {
+	static onPageChange() {
+		this.updateToDo()
+		this.updateCompleted()
+		// TODO: Add Events
+
+		// Update page visibility
+		super.onPageChange()
+	}	
+
+	static updateToDo() {
 		const toDo = document.getElementById("todo")
 		Page.clearChildren(toDo, 1)
+
+		const tasks = Data.tasks.filter((task) => task.progress != 100)
 
 		for (const task of tasks) {
 			toDo.append(List.generateTaskCard(task))
 		}
 	}
 
-	static updateCompleted(tasks) {
+	static updateCompleted() {
 		const completed = document.getElementById("completed")
 		Page.clearChildren(completed, 1)
+
+		const tasks = Data.tasks.filter((task) => task.progress == 100)
 
 		for (const task of tasks) {
 			completed.append(List.generateTaskCard(task))
@@ -27,7 +40,10 @@ class List extends Page {
 		const container = document.createElement("div")
 		container.className = "task-container"
 
-		const mainTask = task.generateTaskCard()
+		const mainTask = task.generateTaskCard(() => {
+			List.updateToDo()
+			List.updateCompleted()
+		})
 		
 		const editButton = document.createElement("div")
 		editButton.className = "glass-panel button"
