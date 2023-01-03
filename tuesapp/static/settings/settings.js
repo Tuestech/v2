80548c0b2402dc9948c702d223dbe302484c5c0c
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2023-01-02 18:18:43 -0600

    Connect settings inputs to data

--- tuesapp/static/settings/settings.js
+++ tuesapp/static/settings/settings.js
@@ -2,6 +2,51 @@ class Settings extends Page {
 	static init() {
 		this.pageName = "settings"
 		this.pageBody = document.getElementById("settings")
+
+		this.preferences_input_id_key_pairs = {
+			"workload-limit": "workloadLimit",
+			"data-collection": "dataCollection",
+			"beta": "beta",
+			"default-links": "defaultLinks",
+			"show-completed": "showCompleted"
+		}
+
+		// Preferences
+		Settings.activatePreferences()
+
 		super.init()
+	}
+
+	static setInputValue(input, value) {
+		if (input.getAttribute("type") == "checkbox") {
+			input.checked = value
+		} else {
+			input.value = value
+		}
+	}
+
+	static getInputValue(input) {
+		if (input.getAttribute("type") == "checkbox") {
+			return input.checked
+		} else {
+			return input.value
+		}
+	}
+
+	static connectInput(input, settingsKey) {
+		input.addEventListener("change", () => {
+			Data.settings[settingsKey] = Settings.getInputValue(input)
+			console.log(Settings.getInputValue(input), Data.settings)
+			Data.requestUpdate()
+		})
+	}
+
+	static activatePreferences() {
+		// Set the current states of inputs and connect with data
+		for (const [id, key] of Object.entries(Settings.preferences_input_id_key_pairs)) {
+			const input = document.getElementById(id)
+			Settings.setInputValue(input, Data.settings[key])
+			Settings.connectInput(input, key)
+		}
 	}
 }
