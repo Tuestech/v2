9bbcd0fd1de44a6491e8da3bf9fbbe17ed4199cb
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2023-01-03 10:45:05 -0600

    Tutorial.js basic framework

--- tuesapp/static/tutorial.js
+++ tuesapp/static/tutorial.js
@@ -1,2 +1,28 @@
 class Tutorial {
+	static step = 1
+
+	static start() {
+		Tutorial.step = 1
+		Tutorial.generateModal()
+	}
+
+	static next() {
+		Tutorial.step += 1
+		Tutorial.generateModal()
+	}
+
+	static generateModal() {
+		switch (Tutorial.step) {
+			case 1:
+				// Make new modal with intro things
+				break;
+			
+			case 2:
+				// Make new modal with tutorial things
+				break;
+			// More cases here
+			default:
+				// Make exit modal
+		}
+	}
 }
