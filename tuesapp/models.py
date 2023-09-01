c957b62759c854763e499e1c0d9eb16b52facb2c
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2023-08-31 21:35:34 -0500

    User metadata migration

--- tuesapp/models.py
+++ tuesapp/models.py
@@ -4,6 +4,7 @@ class User(models.Model):
 	app_data = models.TextField()
 	uid = models.CharField(max_length=255)
 	name = models.CharField(max_length=255)
+	metadata = models.TextField(default="")
 
 	def __str__(self):
 		return f"<User uid='{self.uid}' name='{self.name}'>"
